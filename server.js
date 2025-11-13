916cf214681e0613fff657b408e5701f0400765b
amirmahdighadiri <amirmahdiighadirii@gmail.com>
2025-11-13 21:20:39 +0330

    update json-server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
+});
